f3996e9c65f4f47c03e5274e4ad548616182b910
atempestg <atempestg@gmail.com>
2023-04-23 13:27:01 +0900

    '20230423_08'

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,3 +5,13 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 DEBUG=False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends/postgresql_psycopg2',
+        'NAME': 'pybo',
+        'PASSWORD': ':giEcy%BPnNp1y<[D4WL(~qS83!LjVb`',
+        'HOST': 'ls-2034d4cd826c7810dd78cdc78b7365e1197ad59f.cvkqqskmwouw.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
