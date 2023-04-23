72d2020ae4ddb9193b10717c3d89c6a696c45d43
atempestg <atempestg@gmail.com>
2023-04-23 12:17:29 +0900

    '20230423_06'

--- config/settings/base.py
+++ config/settings/base.py
@@ -178,6 +178,7 @@ LOGGING = {
             'level': 'INFO',
             'filters': ['require_debug_false'],
             'class': 'logging.handlers.RotatingFileHandler',
+            'filename':BASE_DIR / 'logs/mysite.log',
             'maxBytes': 1024*1024*5, # 5MB
             'backupCount': 5,
             'formatter': 'standard',
